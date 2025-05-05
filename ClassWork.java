4b4592c11e405c4eac10d21b05c59cc663d929cf
jchinedu <jchinedu080@gmail.com>
2025-05-05 13:00:32 +0100

    'today'

---
+++ ClassWork.java
@@ -0,0 +1,17 @@
+public class ClassWork {
+ public static void main(String[] args) {
+   int[] score = new int[5];
+    score[0] = 22;
+    score[1] = 51;
+    score[2] = 38;
+    score[3] = 23;
+    score[4] = 48;
+System.out.printf("\t%d %d %d %d %d\n",score[0], score[1], score[2], score[3], score[4]);
+
+
+for(int counter = 0; counter < score.length; counter++) {
+        System.out.printf("%5d\n",score[counter]);
+
+}
+ }
+}
