4af858fca140ac623d0cc3267b6322f08ee209f3
jchinedu <jchinedu080@gmail.com>
2025-05-06 18:41:55 +0100

    'git'

--- testarray.java
+++ testarray.java
@@ -3,17 +3,12 @@ import java.util.Scanner;
 
 public class testarray {
     public static void main(String[] args) {
-        int[] list = {5, 6, 7, 8, 9, 3, 4, 6, 7, 8, 9};
-        //System.out.println(ArrayKata.maximum(list));
-	// System.out.println(ArrayKata.minimum(list));
-	//System.out.print(ArrayKata.sumOf(list));
-	//System.out.print(ArrayKata.sumOfEvenNumbersIn(list));
-	//System.out.print(ArrayKata.sumOfOddNumbersIn(list));
-	//System.out.print(Arrays.toString(ArrayKata.maximumAndMinimumOf(list)));
-	//System.out.println(ArrayKata.noOfOddNumbersIn(list));
-	//System.out.print(ArrayKata.noOfEvenNumbersIn(list));  
-	//System.out.print(Arrays.toString(ArrayKata.evenNumbersIn(list))); 
-	 // System.out.print(Arrays.toString(ArrayKata.oddNumbersIn(list)));
-	  System.out.print(Arrays.toString(ArrayKata.squareNumbersIn(list)));
-    }
+        int[] data = {3, 5, 6, 3, 7, 8, 6, 9};
+
+        int[] result = ArrayCarter.ArrangeArray(Number); 
+        System.out.println(Arrays.toString(result));       
 }
+
+
+}
+
